abd63ee7a105b3910d1ead2c4e0dee29627619d0
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-16 14:35:34 -0800

    Complete gif query from GIPHY
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,33 +1,57 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import TicketMaster from './js/TicketMaster.js';
+import TicketMaster from './services/TicketMaster.js';
+import Giphy from './services/Giphy-services.js';
 
-//Bus Log
+//Business Logic
 
-async function grabEvents(query) {
-  const response = await TicketMaster.getEvents(query);
-  if (response["_embedded"]) {
-    printEvents(response, query);
-  } else {
-    printError(response, query);
+async function getTixData(query) {
+  const responseTix = await TicketMaster.getEvents(query);
+  if (responseTix["_embedded"]) {
+    const gifQuery = responseTix["_embedded"].events[0].name;
+    printTixEvents(responseTix, query);
+    getGiphyData(gifQuery.substring(0,49));
+  } 
+  else {
+    printError(responseTix, query);
   }
 }
 
-
+async function getGiphyData(searchTerm) {
+  const responseGif = await Giphy.getGif(searchTerm);
+  if (responseGif.data) {
+    printGif(responseGif);
+  } 
+  else {
+    printError("Can't locate GIF for this search",searchTerm);
+  }
+}
 
 // UI Logic
 
-function printEvents(response, query) {
+function printTixEvents(responseTix, query) {
+  let newAnchor = document.createElement('a');
+  let newImg = document.createElement('img');
+  let displayImg = responseTix['_embedded'].events[0].images[0].url;
+  newImg.setAttribute("width", "50%");
+  newImg.setAttribute("height", "auto");
   document.querySelector('#showResponse').innerText = `The query for ${query} results in the following events:
-  Event: ${response["_embedded"].events[0].name}
-  Date: ${response['_embedded'].events[0].dates.start.localDate}
-  venue: ${response['_embedded'].events[0]["_embedded"].venues[0].name}
+  Event: ${responseTix["_embedded"].events[0].name}
+  Date: ${responseTix['_embedded'].events[0].dates.start.localDate}
+  venue: ${responseTix['_embedded'].events[0]["_embedded"].venues[0].name}
   `;
-  // date: ${response['_embedded'].events[0].dates.start.localDate}
-  // venue: ${response['_embedded'].events[0].venues[0].name}
-  // Link: ${response['_embedded'].events[0].url}
-  // image: ${response['_embedded'].events[0].images[0]}
+  newAnchor.innerHTML = `<a href="${responseTix['_embedded'].events[0].url}">${responseTix['_embedded'].events[0].url}</a><br>`;
+  document.querySelector('#outputDiv').appendChild(newAnchor);
+  newImg.setAttribute("src", displayImg);
+  document.querySelector('#outputDiv').appendChild(newImg);
+}
+
+function printGif(inputData) {
+  let newGif = document.createElement("img");
+  newGif.src = inputData.data[0].images.downsized.url;
+  newGif.alt = inputData.data[0].title;
+  document.getElementById("outputDiv").append(newGif);
 }
 
 function printError(error, query) {
@@ -38,7 +62,7 @@ function printError(error, query) {
 function handleTicketForm() {
   event.preventDefault();
   const input = document.getElementById("text-input").value;
-  grabEvents(input);
+  getTixData(input);
 }
 
 window.addEventListener("load", function() {
---
+++ src/services/Giphy-services.js
@@ -0,0 +1,15 @@
+export default class Giphy {
+  static async getGif(searchTerm) {
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchTerm}&limit=1&offset=0&rating=g&lang=en`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
