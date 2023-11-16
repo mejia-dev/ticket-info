762f7eeff17984e9e68d682acc45d9203bca0a21
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-16 11:25:40 -0800

    Add TicketMaster and basic display functionality
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import OtherModule from './js/otherModule.js';
+import TicketMaster from './js/TicketMaster.js';
 
-function handleSampleForm() {
+//Bus Log
+
+async function grabEvents(query) {
+  const response = await TicketMaster.getEvents(query);
+  if (response["_embedded"]) {
+    printEvents(response, query);
+  } else {
+    printError(response, query);
+  }
+}
+
+
+
+// UI Logic
+
+function printEvents(response, query) {
+  document.querySelector('#showResponse').innerText = `The query for ${query} results in the following events:
+  Event: ${response["_embedded"].events[0].name}
+  Date: ${response['_embedded'].events[0].dates.start.localDate}
+  venue: ${response['_embedded'].events[0]["_embedded"].venues[0].name}
+  `;
+  // date: ${response['_embedded'].events[0].dates.start.localDate}
+  // venue: ${response['_embedded'].events[0].venues[0].name}
+  // Link: ${response['_embedded'].events[0].url}
+  // image: ${response['_embedded'].events[0].images[0]}
+}
+
+function printError(error, query) {
+  document.querySelector('showResponse').innerText = null;
+  document.querySelector('showResponse').innerText = `An error occurred getting data for ${query}: ${error}`;
+}
+
+function handleTicketForm() {
   event.preventDefault();
-  document.getElementById("outputDiv").innerText = null;
-  const pTag = document.createElement("p");
-  pTag.append(document.getElementById("text-input").value);
-  document.getElementById("outputDiv").append(pTag);
+  const input = document.getElementById("text-input").value;
+  grabEvents(input);
 }
 
 window.addEventListener("load", function() {
-  document.getElementById("sample-form").addEventListener("submit", handleSampleForm);
+  document.getElementById("ticket-form").addEventListener("submit", handleTicketForm);
 });
---
+++ src/js/TicketMaster.js
@@ -0,0 +1,19 @@
+export default class TicketMaster {
+  static async getEvents(query) {
+    try {
+      const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${query}&apikey=${process.env.TICKETMASTER_API_KEY}`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
+
+// https://app.ticketmaster.com/discovery/v2/events.json?postalCode=${zip}&apikey=${process.env.TICKETMASTER_API_KEY}
+
+
